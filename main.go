36ce28c602ec502e1ba97408c7c4ba36c6fa1b60
Nico Audy <nicoaudy1994@gmail.com>
2020-07-15 20:53:59 +0700

    WIP nice pattern when terminate

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"intro/handlers"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 )
 
@@ -26,5 +28,20 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
-	server.ListenAndServe()
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	log.Println("Receive terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(tc)
 }
