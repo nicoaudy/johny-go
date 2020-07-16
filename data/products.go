30a5af4ea70a3374d8d8cf60ad4ca5bb80b79700
Nico Audy <nicoaudy1994@gmail.com>
2020-07-16 11:31:16 +0700

    create sample data

---
+++ data/products.go
@@ -0,0 +1,37 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID		int
+	Name		string
+	Description	string
+	Price		float32
+	SKU		string
+	CreatedAt	string
+	UpdatedAt	string
+	DeletedAt	string
+}
+
+var ProductList = []*Product {
+	&Product{
+		ID:				1,
+		Name:			"Kenangan Mantan",
+		Description:	"Kopi Kenangan Mantan",
+		Price:			1.5,
+		SKU:			"as81",
+		CreatedAt:		time.Now().UTC().String(),
+		UpdatedAt:		time.Now().UTC().String(),
+		DeletedAt:		time.Now().UTC().String(),
+	},
+	&Product{
+		ID:				2,
+		Name:			"Americano",
+		Description:	"Kopi hitam",
+		Price:			1.2,
+		SKU:			"sj1s",
+		CreatedAt:		time.Now().UTC().String(),
+		UpdatedAt:		time.Now().UTC().String(),
+		DeletedAt:		time.Now().UTC().String(),
+	},
+}
